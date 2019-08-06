c0d2aaeac1b9b20bd9a2a14056c4fd99e5899c5e
Felipe Pina <fop.net@gmail.com>
2019-08-06 13:42:38 -0300

    react-vis chart

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -5,6 +5,8 @@ import TopLogo from "../../assets/noun_Graph.png";
 import AmountLogo from "../../assets/noun_solar.png";
 import PanelService from "../../services/panelService";
 import CurrencyFormat from "react-currency-format";
+import { VerticalGridLines, YAxis, XAxis, XYPlot, LineSeries } from "react-vis";
+import "../../../node_modules/react-vis/dist/style.css";
 import "./dashboard.css";
 
 class Dashboard extends Component {
@@ -20,6 +22,7 @@ class Dashboard extends Component {
         zipcode: "",
       },
       top3Amount: [],
+      graphData: [],
     };
   }
 
@@ -28,97 +31,112 @@ class Dashboard extends Component {
 
     const ia = await panelService.getPanelsByState();
     this.setState({ ia: ia });
-    console.log("ia ", this.state.ia);
+    // console.log("ia ", this.state.ia);
 
     const higherCost = await panelService.getPanelsCostByZipcode();
     this.setState({ higherCost: higherCost });
-    console.log("higherCost ", this.state.higherCost);
+    // console.log("higherCost ", this.state.higherCost);
 
     const top3Amount = await panelService.getPanelTop3Amount();
     this.setState({ top3Amount: top3Amount });
-    console.log("topp3Amount ", this.state.top3Amount);
+    // console.log("topp3Amount ", this.state.top3Amount);
+
+    const graphData = await panelService.getPanelsSystemSizeByYear();
+    this.setState({ graphData: graphData });
+    // console.log("graphData ", this.state.graphData);
   }
 
   render() {
     return (
       <div className="container">
-        <div className="cards">
-          <div className="imgBox">
-            <div>
-              <img src={AmountLogo} alt="Amount Installations" />
-              <h2>Amount Installations</h2>
-            </div>
-          </div>
-          <div className="overlay" />
-          <div className="content">
-            <h2>Number of Installations Done by user state</h2>
-            <h3>
-              Informations:
-              <ul>
-                <li>Total Amount: {this.state.ia.amount}</li>
-                <li>User state: {this.state.ia.state}</li>
-              </ul>
-            </h3>
-          </div>
+        <div className="row  ">
+          <XYPlot height={300} width={900}>
+            <LineSeries data={this.state.graphData} />
+            <XAxis />
+            <YAxis />
+            <VerticalGridLines />
+          </XYPlot>
         </div>
 
-        <div className="cards">
-          <div className="imgBox">
-            <div>
-              <img src={CostLogo} alt="Higher Costs" />
-              <h2>Higher Costs</h2>
+        <div className="row">
+          <div className="cards">
+            <div className="imgBox">
+              <div>
+                <img src={AmountLogo} alt="Amount Installations" />
+                <h2>Amount Installations</h2>
+              </div>
+            </div>
+            <div className="overlay" />
+            <div className="content">
+              <h2>Number of Installations Done by user state</h2>
+              <h3>
+                Informations:
+                <ul>
+                  <li>Total Amount: {this.state.ia.amount}</li>
+                  <li>User state: {this.state.ia.state}</li>
+                </ul>
+              </h3>
             </div>
           </div>
-          <div className="overlay" />
-          <div className="content">
-            <h2>Higher cost installation</h2>
-            <h3>
-              Informations:
-              <ul>
-                <li>ZipCode: {this.state.higherCost.zipcode}</li>
-                <li>
-                  Cost:
-                  <CurrencyFormat
-                    value={this.state.higherCost.cost}
-                    displayType={"text"}
-                    thousandSeparator={true}
-                    prefix={"$"}
-                  />
-                </li>
-              </ul>
-            </h3>
-          </div>
-        </div>
 
-        <div className="cards">
-          <div className="imgBox">
-            <div>
-              <img src={TopLogo} alt="Top Amount Installations" />
-              <h2>Top Amount Installations</h2>
+          <div className="cards">
+            <div className="imgBox">
+              <div>
+                <img src={CostLogo} alt="Higher Costs" />
+                <h2>Higher Costs</h2>
+              </div>
+            </div>
+            <div className="overlay" />
+            <div className="content">
+              <h2>Higher cost installation</h2>
+              <h3>
+                Informations:
+                <ul>
+                  <li>ZipCode: {this.state.higherCost.zipcode}</li>
+                  <li>
+                    Cost:
+                    <CurrencyFormat
+                      value={this.state.higherCost.cost}
+                      displayType={"text"}
+                      thousandSeparator={true}
+                      prefix={"$"}
+                    />
+                  </li>
+                </ul>
+              </h3>
             </div>
           </div>
-          <div className="overlay" />
-          <div className="content">
-            <h2>Top 3 largest number of installations</h2>
-            <h3>
-              Informations:
-              <table>
-                <thead>
-                  <tr>
-                    <th>Month</th>
-                    <th>Total Amount</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {this.state.top3Amount.map(it => (
-                    <tr key={it.month}>
-                      <td>{it.month}</td>
-                      <td>{it.amount}</td>
+
+          <div className="cards">
+            <div className="imgBox">
+              <div>
+                <img src={TopLogo} alt="Top Amount Installations" />
+                <h2>Top Amount Installations</h2>
+              </div>
+            </div>
+            <div className="overlay" />
+            <div className="content">
+              <h2>Top 3 largest number of installations</h2>
+              <h3>
+                Informations:
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Month</th>
+                      <th>Total Amount</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
-            </h3>
+                  </thead>
+                  <tbody>
+                    {this.state.top3Amount.map(it => (
+                      <tr key={it.month}>
+                        <td>{it.month}</td>
+                        <td>{it.amount}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </h3>
+            </div>
           </div>
         </div>
       </div>
--- src/services/panelService.js
+++ src/services/panelService.js
@@ -67,5 +67,30 @@ export default class PanelService {
         return result.data.data.panelsCountTop3ByMonth;
       }
     };
+
+    this.getPanelsSystemSizeByYear = async () => {
+      const query = {
+        query: `query  {
+            panelsSystemSizeByYear {
+                      size,
+                      year
+                      }
+                  }`,
+      };
+
+      const result = await api.post("/", query);
+
+      // console.log("result", result);
+
+      if (result.data.errors) {
+        console.error("error ", result.data.errors[0]);
+        throw result.data.errors[0];
+      } else {
+        return result.data.data.panelsSystemSizeByYear.map(coord => ({
+          x: coord.year,
+          y: coord.size,
+        }));
+      }
+    };
   }
 }
